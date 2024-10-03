2c2b4a30084ed6c974229542561b05f4b0c3a295
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-10-02 18:02:44 -0700

    Adds integer specification to entry validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ApplicationRecord
+    
+    validates :calories, :proteins, :carbohydrates, :fats, presence: true, numericality: { only_integer: true }
+    validates :meal_type, presence: true
 
     def day
         self.created_at.strftime("%b %e, %Y")
